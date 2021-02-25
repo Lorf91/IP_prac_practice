53255e3f9706def230ac051c8d53d1ab594cf0b5
Ivan <ivan.91.web@gmail.com>
2021-02-25 22:42:18 +0200

    firs task

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 "use strict";
+
+const numberOfFilms = +prompt('How much film do you see?', '');
+const personalMovieDB = {
+    count : numberOfFilms,
+    movies : {},
+    actors : {},
+    genres : [],
+    privat : false,
+};
+
+const question1 = prompt('What film do see last?', ''),
+      question2 = prompt('Yor mark :', ''),
+      question3 = prompt('What film do see last?', ''),
+      question4 = prompt('Yor mark :', '');
+
+personalMovieDB['movies'][question1] = question2;
+personalMovieDB['movies'][question3] = question4;
