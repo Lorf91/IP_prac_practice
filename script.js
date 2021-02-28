f74af47bdddae5791a12d80a50ab802027fcc7cb
Ivan <ivan.91.web@gmail.com>
2021-02-28 15:55:53 +0200

    practic part_2

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 "use strict";
 
 function runFirstProgram() {
-    const numberOfFilms = +prompt('How much film do you see?', '');
+    let i, a, b, numberOfFilms;
+    
+    numberOfFilms = +prompt('How much film do you see?', '');
+
+    while (numberOfFilms == '' ||  numberOfFilms == null || numberOfFilms.lenth > 50) {
+        console.log('Нужно ввести значение не более 50 символов !');
+        numberOfFilms = +prompt('How much film do you see?', '');
+    }
+
     const personalMovieDB = {
         count: numberOfFilms,
         movies: {},
@@ -9,20 +17,29 @@ function runFirstProgram() {
         genres: [],
         privat: false,
     };
+    
+    
+    for(i = 0; i < 2; i++) {
+        a = prompt('What film do see last?', '');
+        b = prompt('Yor mark :', '');
+        
+        personalMovieDB.movies[a] = b;
+    }
 
-    const question1 = prompt('What film do see last?', ''),
-        question2 = prompt('Yor mark :', ''),
-        question3 = prompt('What film do see last?', ''),
-        question4 = prompt('Yor mark :', '');
-
-    personalMovieDB['movies'][question1] = question2;
-    personalMovieDB['movies'][question3] = question4;
-
+    if (personalMovieDB.count <= 10) {
+        console.log('Вы посмотрели мало фильмов');
+    } else if (personalMovieDB.count > 10 && personalMovieDB.count <= 30) {
+        console.log('Вы классический зритель');
+    } else if (personalMovieDB.count > 30) {
+        console.log('Вы киноман');
+    } else {
+        console.log('Произошла ошибка');
+    }
+    
     console.log(personalMovieDB);
 }
 
 // runFirstProgram();
-
 
 
 
