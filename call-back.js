0790398171744552b20654fd38fe8e8bc087d170
Ivan <ivan.91.web@gmail.com>
2021-02-28 22:17:41 +0200

    +call-back

---
+++ call-back.js
@@ -0,0 +1,13 @@
+"use strict";
+
+function lear(lang, callback) {
+    console.log(`Я учу ${lang}`);
+    callback();
+}
+
+function done() {
+    console.log('Я прошел этот урок');
+}
+
+lear('Java Script', done);
+
